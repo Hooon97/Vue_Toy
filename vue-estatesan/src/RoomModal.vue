7d2da54a4116ae18e0a113b03c4a7aae7a686890
unknown <ehgns017@gmail.com>
2024-02-11 21:34:46 +0900

    Vue3_estate/watch 1.watch는 데이터가 변화하는 것을 감지하는 vue 제공 기능이다. 2.감시하고자 하는 데이터 이름() {} 형식으로 사용 가능하다. 3.자바스크립트에는 isNaN() 이라고 숫자인지 아닌지 검증하는 메서드가 있다. 4.Vue 잘하고 싶으면 자스도 잘해야하는듯

--- vue-estatesan/src/RoomModal.vue
+++ vue-estatesan/src/RoomModal.vue
@@ -6,11 +6,7 @@
       <h4>{{rooms[index].title}}</h4>
       <p>{{rooms[index].content}} </p>
       <input v-model="month">
-      <!-- <input @input="month = $event.target.value"> -->
       <p> {{month}}개월 선택함 : {{ rooms[index].price * month}}  원 </p>
-      <!-- props에서 받아온 데이터는 항상 read-only 이므로, 수정이 불가능하다. 
-        대신 emit 문법을 사용하면 부모 컴포넌트에게 이벤트 실행 요청을 보낼 수 있다.
-      -->
       <button @click="$emit('closeModal')"> 닫기 </button>
     </div>
   </div>
@@ -21,8 +17,6 @@ import DiscountBanner from './DiscountBanner.vue'
 
 export default {
     name:'RoomModal',
-    //부모로부터 받은 데이터, 이름 : 데이터 형식
-    //props에서 받아온 데이터는 항상 read-only 이므로, 수정이 불가능하다.
     props : {
         rooms: Array,
         index : Number,
@@ -34,8 +28,22 @@ export default {
     },
     components : {
         DiscountBanner,
-    }
+    },
+    watch : {
+      month(number) {
+        if(number >= 13){
+          alert("13이상은 안됨")
+        }else if(!isNaN(number)){
+          this.month = number;
+        }
+        else{
+          alert("숫자만 입력하셈")
+          this.month = 1;
+        }
+      },
+    },
 }
+    
 </script>
 
 <style>
