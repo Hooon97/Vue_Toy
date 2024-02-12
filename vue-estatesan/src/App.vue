2051a88caaf7d7f296107e8004b52448ba8f3388
unknown <ehgns017@gmail.com>
2024-02-12 14:19:15 +0900

    Vue3_estate/sorting 1. Sorting 함수를 다루는 능력은 Vue 문법이 아니라 자바스크립트를 다루는 영역임 2. Shallow Copy가 아니라 Deep copy를 하고 싶으면 [...arrayName]을 하면 됨 3. 요즘은 데이터 원본은 보호하는 코딩 방식이 유행임. sorting 등의 기능 구현 시 참고하기

--- vue-estatesan/src/App.vue
+++ vue-estatesan/src/App.vue
@@ -1,12 +1,9 @@
 <template>
+    <button @click="priceSortDesc()"> 가격 높은 순 정렬 </button>
+    <button @click="priceSortAsc()"> 가격 낮은 순 정렬 </button>
+    <button @click="priceSortTitle()"> 제목 순 정렬 </button>
+    <button @click="sortBack()"> 되돌리기 </button>
 
-  <!-- 
-  <div class = "start" :class="{end : modalFlag}">
-
-  </div>
-  -->
-
-    <!-- Vue 제공 animation css -->
     <transition name="fade">
     <RoomModal v-if="modalFlag" 
        :rooms="products" :index="curModIdx" :modalFlag="modalFlag"
@@ -24,15 +21,12 @@
     @openModal = "modalFlag = true; curModIdx = $event"
     :room="product" />
   
-  
 </template>
 
 <script>
 //문법만 외우면 코딩 인생 망함 ㅋㅋㅋ
 //그 문법을 언제, 왜 쓰는지 알아야함ㅋㅋㅋ
 
-//외부 파일의 데이터, 함수 등을 가져와 사용할 때는 import를 통해 사용할 수 있다.
-//단, import 하고자 하는 파일도 export가 정의되어 있어야 한다.
 import oneroomData from './assets/data.js'
 import DiscountBanner from './DiscountBanner.vue'
 import RoomModal from './RoomModal.vue'
@@ -45,6 +39,7 @@ export default {
   data(){
     return {
       //속성 데이터 바인딩
+      origin_products : [...oneroomData],
       스타일: "color : red",
       products : oneroomData,
       menus : ['Home', 'Shop', 'About'],
@@ -58,18 +53,28 @@ export default {
       this.curModIdx = product_idx;
       this.modalFlag = true;
     },
+    priceSortAsc(){
+      this.products.sort(function(a,b){
+        return a.price-b.price;
+      });
+    },
+    priceSortDesc(){
+      this.products.sort(function(a,b){
+        return b.price-a.price;
+      });
+    },
+    priceSortTitle(){
+      this.products.sort(function(a,b){
+        return a.title > b.title ? 1 : -1;
+      });
+    },
+    sortBack(){
+      // deep copy
+      this.products = [...this.origin_products];
+    },
   },
-
-  /* 그래서 이거 왜 씀?
-  1. 코드 축약이 가능하기 때문이다. -> 단위 코드로 해석이 가능하여 복잡성 감소
-  2. 코드 재사용에 용이하다.
-  3. 코드 관리가 용이함. (수정, 유지 보수)
-  컴포넌트는 무조건 많이 만든다고 좋은 게 아니다. 어떤 상황에선 데이터 기입에 문제가 발생할 수 있다.
-  초보자 특) 무조건 컴포넌트화 시키려고 함
-  */
   components: {
-    //왼쪽은 자유 작명(내가 사용할 이름), 오른쪽은 import 해올 때 사용한 이름
-    // ES6 문법에서는, 좌우측의 이름이 같으면 하나로 축약 가능 ex) Discount
+
     DiscountBanner, // DiscountBanner : DiscountBanner
     RoomModal,
     RoomCard,
